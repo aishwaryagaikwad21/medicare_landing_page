f7819bade1bb57a74b5095217ad55f957963f6a8
anurag gharat <anuraggharat55@gmail.com>
2020-02-29 22:27:35 +0530

    chatsystem

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -1,46 +1,68 @@
 <template>
   <div>
     <v-app-bar
-      color="deep-purple accent-4"
+      class="primary-light"  
       dense
       dark
+      fixed
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    
 
       <v-toolbar-title>Page title</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
+            <div class="numberofpeople">
+               <div class="mt-1">78</div> 
+            </div>
+        
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+        <v-btn class="mx-2  py-2" dark small color="primary">
+              
+            Leave Room
+        
+        </v-btn>
 
-      <v-menu
-        left
-        bottom
-      >
-        <template v-slot:activator="{ on }">
-          <v-btn icon v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
 
-        <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            @click="() => {}"
-          >
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+    
     </v-app-bar>
-  </div>
+
+    <div  class="message-text  mt-12 ">
+          <!-- <li v-for="index in 10" :key="index">
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus soluta, modi quia deserunt ratione molestias rem nesciunt, accusamus id reprehenderit porro, eveniet nobis dolor nihil mollitia odit doloribus pariatur quos.</div>
+        </li> -->
+        <div v-for="index in 10" :key="index">
+            <!-- senderhai -->
+        <div  class="sender-box mr-5 py-2 pr-5 pl-5 ">
+
+            jatinda.kjvdajsvasvkhasvads
+            
+            </div>
+
+        <!-- reciver -->
+        <div class="receiver-box   ml-5 py-2 pr-5 pl-5"> 
+            djasldhaksgd
+            
+            </div>
+        </div>
+    </div>
+
+       
+
+    <v-form     class="chat-message  "> 
+        <v-container fluid>
+            <v-text-field
+                v-model="message2"
+                solo
+                label="Enter Message"
+                clearable
+            >
+           
+            </v-text-field>
+            
+        </v-container>
+    </v-form>   
+ </div>
 </template>
 <script>
 export default {
@@ -48,5 +70,49 @@ export default {
 }
 </script>
 <style scoped>
+@import "../assets/colors.css";
 
+.chat-message{
+       position: fixed;                  /* added */
+   bottom:0;                           /* added */
+   left:0; 
+   width:100%;
+}
+
+.numberofpeople{
+    background-color: red;
+    height: 30px;
+    width:30px;
+    text-align: center;
+    vertical-align: middle !important;
+    border-radius: 100%
+}
+
+.message-text{
+    height: 100vh;
+    width: 100%;
+    overflow-y: scroll;
+    background-color: green;
+    overflow: auto;
+    padding-bottom: 30vh;
+    padding-bottom: 30vh;
+}
+
+.sender-box{
+
+    margin-top: 10px;
+    border: 2px solid red;
+    float: right;
+    text-align: right;
+    border-radius: 20px;
+    margin-left: 100% !important;
+}
+.receiver-box{
+    margin-top: 10px;
+    border: 2px solid red;
+    float: left;
+    text-align: left;
+    border-radius: 20px;
+    margin-right: 100% !important;
+}
 </style>
